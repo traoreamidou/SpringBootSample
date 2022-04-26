893d793e8d933c5a6766d1f608481390143f7c65
Hamidou Traore <traoreamidou6729@gmail.com>
2022-04-26 19:50:57 +0200

    Third commit

--- src/main/java/com/example/domain/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/domain/user/service/impl/UserServiceImpl.java
@@ -27,4 +27,9 @@ public class UserServiceImpl implements UserService{
 		return userMapper.findMany();
 	}
 
+	@Override
+	public MUser getUserOne(String userId) {
+		return userMapper.findOne(userId);
+	}
+
 }
--- src/main/java/com/example/repository/UserMapper.java
+++ src/main/java/com/example/repository/UserMapper.java
@@ -12,4 +12,6 @@ public interface UserMapper {
 	public int insertOne(MUser user);
 	
 	public List<MUser> findMany();
+	
+	public MUser findOne(String userId);
 }
